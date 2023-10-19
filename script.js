82dccdeebc4d72c916712bd20390d7025cb3db33
Kevin Oudai <46614071+Kevin-Oudai@users.noreply.github.com>
2023-10-19 13:16:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
 function calculateBill() {
     const kWh = parseFloat(document.getElementById('kWh').value);
-    let cost = 0;
+    const lastBiMonthlyBill = parseFloat(document.getElementById('lastBill').value || 0);
+    const lastMonthlyBill = lastBiMonthlyBill / 2;
+    let tierCost = 0;
+    let tierText = '';
 
     if (kWh <= 200) {
-        cost = kWh * 0.28;
+        tierCost = kWh * 0.28;
+        tierText = `Tier 1 (0.28 cents/kWh): $${tierCost.toFixed(2)}`;
     } else if (kWh <= 700) {
-        cost = 200 * 0.28 + (kWh - 200) * 0.40;
+        tierCost = 200 * 0.28 + (kWh - 200) * 0.40;
+        tierText = `Tier 1 (0.28 cents/kWh): $${(200 * 0.28).toFixed(2)}<br>
+                    Tier 2 (0.40 cents/kWh): $${((kWh - 200) * 0.40).toFixed(2)}`;
     } else if (kWh <= 1400) {
-        cost = 200 * 0.28 + 500 * 0.40 + (kWh - 700) * 0.54;
+        tierCost = 200 * 0.28 + 500 * 0.40 + (kWh - 700) * 0.54;
+        tierText = `Tier 1 (0.28 cents/kWh): $${(200 * 0.28).toFixed(2)}<br>
+                    Tier 2 (0.40 cents/kWh): $${(500 * 0.40).toFixed(2)}<br>
+                    Tier 3 (0.54 cents/kWh): $${((kWh - 700) * 0.54).toFixed(2)}`;
     } else {
-        cost = 200 * 0.28 + 500 * 0.40 + 700 * 0.54 + (kWh - 1400) * 0.68;
+        tierCost = 200 * 0.28 + 500 * 0.40 + 700 * 0.54 + (kWh - 1400) * 0.68;
+        tierText = `Tier 1 (0.28 cents/kWh): $${(200 * 0.28).toFixed(2)}<br>
+                    Tier 2 (0.40 cents/kWh): $${(500 * 0.40).toFixed(2)}<br>
+                    Tier 3 (0.54 cents/kWh): $${(700 * 0.54).toFixed(2)}<br>
+                    Tier 4 (0.68 cents/kWh): $${((kWh - 1400) * 0.68).toFixed(2)}`;
     }
 
-    cost += 7.50;  // Add standard charge
-    cost += cost * 0.125;  // Add VAT
+    const vatAmount = (tierCost + 7.50) * 0.125;
+    const currentMonthlyBill = tierCost + 7.50 + vatAmount;
 
-    document.getElementById('result').innerHTML = `Total Bill Amount: $${cost.toFixed(2)}`;
+    const increasePercentage = ((currentMonthlyBill - lastMonthlyBill) / lastMonthlyBill) * 100;
+
+    document.getElementById('increaseValue').innerText = `${increasePercentage.toFixed(2)}%`;
+    document.getElementById('tierCharge').innerHTML = tierText;
+    document.getElementById('vat').innerHTML = `VAT (12.5%): $${vatAmount.toFixed(2)}`;
+    document.getElementById('total').innerHTML = `<strong>Total Bill Amount: $${currentMonthlyBill.toFixed(2)}</strong>`;
 }
